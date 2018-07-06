ba4344ea481942ef65d7a6f36cddb7d4e6885a45
whj826069748 <39262392+whj826069748@users.noreply.github.com>
2018-07-06 16:37:36 +0800

    1

--- config-client/src/main/java/com/example/configserver/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configserver/ConfigClientApplication.java
@@ -3,7 +3,6 @@ package com.example.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableDiscoveryClient
 @SpringBootApplication
